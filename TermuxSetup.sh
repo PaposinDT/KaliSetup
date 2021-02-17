baaf3ed9141593e77076ed37744d7aeb7c4069e1
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-17 18:11:55 +0100

    Add files via upload

---
+++ TermuxSetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Version 1.2
+# coded By Riccardo Papa
+# copyright(please read the license before sharing the code)
+clear
+echo """
+		 ____________________________________________
+		|			                                 |
+		|        The Code By :- Riccardo Papa        |
+		|	     Telegram :- @PaposinDT              |
+		|        Facebook :- https://cutt.us/kfNBv   |
+		|        Youtube :-  https://cutt.us/fp49D   |
+		|        Git Hub :- github.com/PaposinDT     |
+        |        Instagram:- https://cutt.us/2ZDRu   |
+		|___________________________________________ | """
+echo "                           	Version 1.1" 
+echo "                    	  Coded By Riccardo Papa"
+echo "            	  Want to support me?  paypal.me/ricky2006 "
+echo "			This tool is created for educational purpose only!
+ 			We use it to test under NDA agreements with clients and their consents
+  		and we never encourage to misuse or take responsibility for any damage caused !"
+echo " Installing..."
+sudo bash Tools.sh
---
+++ Tools.sh
@@ -0,0 +1,10 @@
+echo "INSTALLING INFORMATION-GATHERING TOOLS"
+mkdir Tools
+sudo bash Information-Gathering.sh
+cd Tools
+git clone https://github.com/capture0x/XCTR-Hacking-Tools/
+git clone https://github.com/tiagorlampert/sAINT.git
+git clone https://github.com/jofpin/trape.git
+git clone https://github.com/georgenicolaou/androidlockcracker.git
+git clone https://github.com/3xploitGuy/lazyaircrack.git
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
