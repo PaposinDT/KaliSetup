b5c44f18caec7ee1c768aa6a251abb608b7cf6db
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-17 14:58:11 +0100

    Add files via upload

---
+++ DDOS.sh
@@ -0,0 +1,13 @@
+echo "INSTALLING DDOS TOOLS"
+mkdir DDOS
+cd DDOS
+git clone https://github.com/5inco/PHDoS
+git clone https://github.com/H1R0GH057/Anonymous
+git clone https://github.com/Hydra7/Planetwork-DDOS
+git clone https://github.com/foreni-packages/t50.git
+wget https://raw.githubusercontent.com/GinjaChris/pentmenu/master/pentmenu
+git clone https://github.com/Anlos0023/webdos-ddoser.git
+git clone https://github.com/gkbrk/slowloris.git
+git clone https://github.com/Anlos0023/webdos-ddoser.git
+git clone https://github.com/XCHADXFAQ77X/XERXES.git
+echo "INSTALLATION COMPLETED ENJOY YOUR HACKING TOOLS"
