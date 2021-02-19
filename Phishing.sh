990d835d1c8fd283fbec2c11799c95cdc43c08af
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-19 11:48:51 +0100

    Update Phishing.sh

--- Phishing.sh
+++ Phishing.sh
@@ -1,5 +1,6 @@
 echo "INSTALLING PHISHING TOOLS"
 mkdir Phishing
+bash Information-Gathering.sh
 cd Phishing
 git clone https://github.com/xHak9x/SocialPhish.git
 git clone https://github.com/UndeadSec/SocialFish.git
