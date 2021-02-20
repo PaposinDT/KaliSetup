1988fda56d4e280ba35157a66adac934b35deebe
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-20 14:31:39 +0100

    Update DDOS.sh

--- DDOS.sh
+++ DDOS.sh
@@ -1,9 +1,9 @@
 echo "INSTALLING DDOS TOOLS"
 mkdir DDOS
 cd DDOS
-git clone https://github.com/5inco/PHDoS
-git clone https://github.com/H1R0GH057/Anonymous
-git clone https://github.com/Hydra7/Planetwork-DDOS
+git clone https://github.com/5inco/PHDoS.git
+git clone https://github.com/H1R0GH057/Anonymous.git
+git clone https://github.com/Hydra7/Planetwork-DDOS.git
 git clone https://github.com/foreni-packages/t50.git
 git clone https://github.com/Anlos0023/webdos-ddoser.git
 git clone https://github.com/gkbrk/slowloris.git
