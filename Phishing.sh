302b719549a2d8f4d9a439bc2c170ffe662e8516
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-21 20:33:29 +0100

    Delete Phishing.sh

--- Phishing.sh
+++
@@ -1,8 +0,0 @@
-echo "INSTALLING PHISHING TOOLS"
-mkdir Phishing
-bash Information-Gathering.sh
-cd Phishing
-git clone https://github.com/xHak9x/SocialPhish.git
-git clone https://github.com/UndeadSec/SocialFish.git
-git clone https://github.com/rsmusllp/king-phisher.git
-git clone https://github.com/BiZken/PhishMailer.git
