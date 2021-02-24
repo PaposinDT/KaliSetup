593ff347d9cf66d05c3584ea66f2bbb1adade270
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-24 13:45:39 +0100

    Update Hacking.sh

--- Hacking.sh
+++ Hacking.sh
@@ -4,22 +4,24 @@ mkdir ./Hacking
 cd ./Hacking
 echo "INSTALLING HACKING TOOLS"
 sleep 3
-echo "Cloning 6 repositories"
+echo "Cloning 7 repositories"
 sleep 2
-echo "Cloning hacktronian:           |-|-|-|-|-|-|-|-|"
+echo "Cloning hacktronian:           |-|-|-|-|-|-|-|-|-|"
 git clone https://github.com/thehackingsage/hacktronian.git --quiet
-echo "Cloning onex:                  |-|-|-|-|-|-|-|"
+echo "Cloning onex:                  |-|-|-|-|-|-|-|-|"
 git clone https://github.com/rajkumardusad/onex.git --quiet
-echo "Cloning hackerpro:             |-|-|-|-|-|-|"
+echo "Cloning hackerpro:             |-|-|-|-|-|-|-|"
 git clone https://github.com/jaykali/hackerpro.git --quiet
-echo "Cloning infect:                |-|-|-|-|-|"
+echo "Cloning infect:                |-|-|-|-|-|-|"
 git clone https://github.com/noob-hackers/infect.git --quiet
-echo "Cloning grabcam:               |-|-|-|-|"
+echo "Cloning grabcam:               |-|-|-|-|-|"
 git clone https://github.com/noob-hackers/grabcam.git --quiet
-echo "Cloning igbrute:               |-|-|-|"
+echo "Cloning igbrute:               |-|-|-|-|"
 git clone https://github.com/dark-player/igbrute.git --quiet
-echo "Cloning Apktool:               |-|-|"
+echo "Cloning Apktool:               |-|-|-|"
 git clone https://github.com/iBotPeaches/Apktool.git --quiet
-echo "Cloning lynis:                 |-|"
+echo "Cloning lynis:                 |-|-|"
 git clone https://github.com/CISOfy/lynis --quiet
+echo "Cloning FotoSploit:                 |-|"
+git clone https://github.com/Cesar-Hack-Gray/FotoSploit.git --quiet
 sleep 2
