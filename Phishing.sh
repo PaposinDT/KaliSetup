8ef3847207d14a5438d733724018cbf10511ae16
Riccardo Papa <rickypapa2006@gmail.com>
2021-02-28 09:29:19 +0100

    Update Phishing.sh

--- Phishing.sh
+++ Phishing.sh
@@ -4,16 +4,18 @@ mkdir ./Phishing
 cd ./Phishing
 echo "INSTALLING PHISHING TOOLS"
 sleep 3
-echo "Cloning 5 repositories"
+echo "Cloning 6 repositories"
 sleep 2
-echo "Cloning SocialPhish:              |-|-|-|-|-|-|"
+echo "Cloning SocialPhish:              |-|-|-|-|-|-|-|"
 git clone https://github.com/xHak9x/SocialPhish.git --quiet
-echo "Cloning SocialFish:               |-|-|-|-|-|"
+echo "Cloning SocialFish:               |-|-|-|-|-|-|"
 git clone https://github.com/UndeadSec/SocialFish.git --quiet
-echo "Cloning king-phisher:             |-|-|-|"
+echo "Cloning king-phisher:             |-|-|-|-|"
 git clone https://github.com/rsmusllp/king-phisher.git --quiet
-echo "Cloning PhishMailer:              |-|-|"
+echo "Cloning PhishMailer:              |-|-|-|"
 git clone https://github.com/BiZken/PhishMailer.git --quiet
-echo "Cloning HiddenEye:                |-|"
+echo "Cloning HiddenEye:                |-|-|"
 git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy.git --quiet
+echo "Cloning Phisher:                  |-|"
+git clone https://github.com/yezz123/Phisher --quiet
 sleep 2
