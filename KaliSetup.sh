d9a6f685f8f0f429a8cab82eba7f5015d87c54fa
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 14:21:04 +0200

    Update KaliSetup.sh

--- KaliSetup.sh
+++ KaliSetup.sh
@@ -5,6 +5,7 @@
 #Version......: 2.4
 #Usage........: sudo ./KaliSetup.sh
 # copyright(please read the license before sharing the code)
+
 clear
 echo """
 			 ____________________________________________
@@ -15,13 +16,14 @@ echo """
 			|        Youtube :-  https://cutt.us/fp49D   |
 			|        Git Hub :- github.com/PaposinDT     |
             		|        Instagram:- https://cutt.us/2ZDRu   |
-			|___________________________________________ | """
+			|___________________________________________ |
+"""
+
 sleep 2
 echo "					Version 1.1
                     	 	 Coded By Riccardo Papa
            	 	Want to support me?  paypal.me/ricky2006 
-		   This tool is created for educational purpose only!" \  #
-#
+		   This tool is created for educational purpose only!"
 echo 
 sleep 1
 echo "The installation speed depends on the signal strength."
@@ -33,40 +35,77 @@ sleep 1
 echo "Installing..."
 echo
 sleep 1
-mkdir ./Tools
+
+mkdir -p ./Tools
 ./Hacking.sh
-cd ./Tools
+cd ./Tools || { echo "Failed to enter Tools directory"; exit 1; }
+
 echo
 echo "INSTALLING GENERIC TOOLS"
 sleep 2
 echo "Cloning 14 repositories"
 echo
 sleep 2
-echo "Cloning airgeddon:                   |-|-|-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git --quiet
-echo "Cloning XCTR-Hacking-Tools:   	     |-|-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/capture0x/XCTR-Hacking-Tools/ --quiet
-echo "Cloning sAINT:                	     |-|-|-|-|-|-|-|-|-|-|-|" 
-git clone https://github.com/tiagorlampert/sAINT.git --quiet
-echo "Cloning trape:              	     |-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/jofpin/trape.git --quiet
-echo "Cloning androidlockcracker:  	     |-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/georgenicolaou/androidlockcracker.git --quiet
-echo "Cloning lazyaircrack:         	     |-|-|-|-|-|-|-|-|"
-git clone https://github.com/3xploitGuy/lazyaircrack.git --quiet
-echo "Cloning setoolkit:                   |-|-|-|-|-|-|-|"
-git clone https://github.com/trustedsec/social-engineer-toolkit/ setoolkit/ --quiet
-echo "Cloning stup:  	                     |-|-|-|-|-|-|"
-git clone https://github.com/iridakos/stup.git --quiet
-echo "Cloning vuls:                 	     |-|-|-|-|-|" 
-git clone https://github.com/future-architect/vuls.git --quiet
-echo "Cloning bash-prompt-generator:       |-|-|-|-|"
-git clone https://github.com/Scriptim/bash-prompt-generator.git --quiet
-echo "Cloning blackhat-arsenal-tools:      |-|-|-|"
-git clone https://github.com/toolswatch/blackhat-arsenal-tools.git --quiet
-echo "Cloning Xeexe:         		     |-|-|"
-git clone https://github.com/persianhydra/Xeexe-TopAntivirusEvasion.git --quiet
-echo "Cloning http-req-smuggling:          |-|"
-git clone https://github.com/anshumanpattnaik/http-request-smuggling.git --quiet
-sleep 2
+
+clone_repo() {
+  local url=$1
+  local folder=$2
+  local name=$3
+
+  # Se non specificato, folder = nome repo senza .git
+  if [ -z "$folder" ]; then
+    folder=$(basename "${url%.git}")
+  fi
+
+  if [ -d "$folder" ]; then
+    echo "Skipping $name (already cloned)"
+    return
+  fi
+
+  # Controlla che il repo esista (200 OK)
+  repo_url="${url%.git}"
+  status=$(curl -o /dev/null -s -w "%{http_code}" "$repo_url")
+
+  if [ "$status" -ne 200 ]; then
+    echo "Repository $name not found or inaccessible (HTTP $status). Skipping."
+    return
+  fi
+
+  echo "Cloning $name: $folder"
+  git clone "$url" "$folder" --quiet
+}
+
+clone_repo "https://github.com/v1s1t0r1sh3r3/airgeddon.git" "" "airgeddon"
+clone_repo "https://github.com/capture0x/XCTR-Hacking-Tools.git" "" "XCTR-Hacking-Tools"
+clone_repo "https://github.com/tiagorlampert/sAINT.git" "" "sAINT"
+clone_repo "https://github.com/jofpin/trape.git" "" "trape"
+clone_repo "https://github.com/georgenicolaou/androidlockcracker.git" "" "androidlockcracker"
+clone_repo "https://github.com/3xploitGuy/lazyaircrack.git" "" "lazyaircrack"
+clone_repo "https://github.com/trustedsec/social-engineer-toolkit.git" "setoolkit" "setoolkit"
+clone_repo "https://github.com/iridakos/stup.git" "" "stup"
+clone_repo "https://github.com/future-architect/vuls.git" "" "vuls"
+clone_repo "https://github.com/Scriptim/bash-prompt-generator.git" "" "bash-prompt-generator"
+clone_repo "https://github.com/toolswatch/blackhat-arsenal-tools.git" "" "blackhat-arsenal-tools"
+clone_repo "https://github.com/persianhydra/Xeexe-TopAntivirusEvasion.git" "" "Xeexe"
+clone_repo "https://github.com/anshumanpattnaik/http-request-smuggling.git" "" "http-req-smuggling"
+sleep 1
+
+cd ..
+echo "Removing unnecessary files."
+sleep 1
+rm -r Hacking.sh
+rm -r LICENSE
+rm -r README.md
+rm -r KaliSetup.sh
+rm -r Phishing.sh
+rm -r Information-Gathering.sh
+rm -r DDoS.sh
+rm -r BruteForce.sh
+sleep 1
+
+echo "Renaming Folder into "Ethical-Hacking"."
+cd ..
+mv KaliSetup Ethical-Hacking
+sleep 1
+
 echo "INSTALLATION COMPLETED. IF YOU LIKE THE SCRIPT, PLEASE LEAVE A STAR ON THE GITHUB REPOSITORY!"
