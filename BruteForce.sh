d6e59288056eb163650ae2f697e9d321ddcab5bd
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 14:15:28 +0200

    Update BruteForce.sh

--- BruteForce.sh
+++ BruteForce.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 #Author.......: Riccardo Papa
-mkdir ./BruteForce
+
+mkdir -p ./BruteForce
 cd ./BruteForce
+
 echo "INSTALLING BRUTEFORCE TOOLS"
 sleep 2
 echo "Cloning 12 repositories"
 echo
 sleep 2
-echo "Cloning Insta-Brute:                 |-|-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/Bitwise-01/Instagram-.git --quiet
-echo "Cloning BruteX:                      |-|-|-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/1N3/BruteX.git --quiet
-echo "Cloning ssb:     		     |-|-|-|-|-|-|-|-|-|-|" 
-git clone https://github.com/kitabisa/ssb.git --quiet
-echo "Cloning bruteforce-database:         |-|-|-|-|-|-|-|-|-|"
-git clone https://github.com/duyet/bruteforce-database.git --quiet
-echo "Cloning Dictionary-Of-Pentesting:    |-|-|-|-|-|-|-|-|"
-git clone https://github.com/insightglacier/Dictionary-Of-Pentesting.git --quiet
-echo "Cloning BruteSploit:                 |-|-|-|-|-|-|-|"
-git clone https://github.com/Screetsec/BruteSploit.git --quiet
-echo "Cloning Google-Password:             |-|-|-|-|-|-|"
-git clone https://github.com/Gioelio/Hack-Google-Password.git --quiet
-echo "Cloning fsociety:                    |-|-|-|-|-|"
-git clone https://github.com/Manisso/fsociety.git --quiet
-echo "Cloning ZIP-BruteForcer:             |-|-|-|-|"
-git clone https://github.com/The404Hacking/ZIP-Password-BruteForcer.git --quiet
-echo "Cloning URLBrute-Py:                 |-|-|-|"
-git clone https://github.com/ReddyyZ/URLBrute-Py.git --quiet
-echo "Cloning thc-hydra:             	     |-|-|"
-git clone https://github.com/vanhauser-thc/thc-hydra.git --quiet
-echo "Cloning pydictor:             	     |-|"
-git clone https://github.com/LandGrey/pydictor.git --quiet
+
+clone_repo() {
+  local url=$1
+  local folder=$2
+  local name=$3
+
+  # Controlla se la cartella esiste già
+  if [ -d "$folder" ]; then
+    echo "Skipping $name (already cloned)"
+    return
+  fi
+
+  # Controlla se la repo esiste (status HTTP 200)
+  status=$(curl -o /dev/null -s -w "%{http_code}\n" "$url")
+
+  if [ "$status" -ne 200 ]; then
+    echo "Repository $name not found or inaccessible (HTTP $status). Skipping."
+    return
+  fi
+
+  echo "Cloning $name: $folder"
+  git clone "$url" --quiet
+}
+
+clone_repo "https://github.com/Bitwise-01/Instagram-.git" "Instagram-" "Insta-Brute"
+clone_repo "https://github.com/1N3/BruteX.git" "BruteX" "BruteX"
+clone_repo "https://github.com/kitabisa/ssb.git" "ssb" "ssb"
+clone_repo "https://github.com/duyet/bruteforce-database.git" "bruteforce-database" "bruteforce-database"
+clone_repo "https://github.com/insightglacier/Dictionary-Of-Pentesting.git" "Dictionary-Of-Pentesting" "Dictionary-Of-Pentesting"
+clone_repo "https://github.com/Screetsec/BruteSploit.git" "BruteSploit" "BruteSploit"
+clone_repo "https://github.com/Gioelio/Hack-Google-Password.git" "Hack-Google-Password" "Google-Password"
+clone_repo "https://github.com/Manisso/fsociety.git" "fsociety" "fsociety"
+clone_repo "https://github.com/The404Hacking/ZIP-Password-BruteForcer.git" "ZIP-Password-BruteForcer" "ZIP-BruteForcer"
+clone_repo "https://github.com/ReddyyZ/URLBrute-Py.git" "URLBrute-Py" "URLBrute-Py"
+clone_repo "https://github.com/vanhauser-thc/thc-hydra.git" "thc-hydra" "thc-hydra"
+clone_repo "https://github.com/LandGrey/pydictor.git" "pydictor" "pydictor"
+
+sleep 2
