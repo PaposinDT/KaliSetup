c011115a55dc7ff8acca884d0a8cf74b453be6ba
Riccardo Papa <rickypapa2006@gmail.com>
2025-06-25 15:03:44 +0200

    Update DDoS.sh

--- DDoS.sh
+++ DDoS.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
-#Author.......: Riccardo Papa
+# Author.......: Riccardo Papa
 
 mkdir -p ./DDoS
 ./BruteForce.sh
 cd ./DDoS
 
-echo
-echo "INSTALLING DDOS TOOLS"
-sleep 2
-echo "Cloning 18 repositories"
-echo
-sleep 2
+spinner() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while kill -0 $pid 2>/dev/null; do
+        for (( i=0; i<${#spinstr}; i++ )); do
+            printf "\r%s [%c]" "$spinner_message" "${spinstr:$i:1}"
+            sleep $delay
+        done
+    done
+    printf "\r%s [✔]\n" "$spinner_message"
+}
 
 clone_repo() {
   local url=$1
   local folder=$2
   local name=$3
 
-  # Se cartella già esiste, skip
   if [ -d "$folder" ]; then
     echo "Skipping $name (already cloned)"
     return
   fi
 
-  # Controlla se la repo esiste (usiamo la url senza .git per la verifica)
+  # Per il controllo uso URL senza .git perché alcune repo lo richiedono
   repo_url="${url%.git}"
   status=$(curl -o /dev/null -s -w "%{http_code}" "$repo_url")
 
@@ -32,9 +37,17 @@ clone_repo() {
     return
   fi
 
-  echo "Cloning $name: $folder"
-  git clone "$url" --quiet
+  spinner_message="Cloning $name"
+  git clone "$url" --quiet &
+  spinner
 }
+
+echo
+echo "INSTALLING DDOS TOOLS"
+sleep 2
+echo "Cloning 18 repositories"
+echo
+sleep 2
 
 clone_repo "https://github.com/5inco/PHDoS.git" "PHDoS" "PHDoS"
 clone_repo "https://github.com/H1R0GH057/Anonymous.git" "Anonymous" "Anonymous"
@@ -44,4 +57,8 @@ clone_repo "https://github.com/Anlos0023/webdos-ddoser.git" "webdos-ddoser" "web
 clone_repo "https://github.com/gkbrk/slowloris.git" "slowloris" "slowloris"
 clone_repo "https://github.com/jseidl/GoldenEye.git" "GoldenEye" "GoldenEye"
 clone_repo "https://github.com/grafov/hulk.git" "hulk" "hulk"
-clone_repo "https://github.com/Leeon123/CC-attack.git_
+clone_repo "https://github.com/Leeon123/CC-attack.git" "CC-attack" "CC-attack"
+# Aggiungi altre repo qui se vuoi
+
+sleep 2
+
